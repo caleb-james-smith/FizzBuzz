de2d04beb0e00d3bf0142145b4c5ee9596c13667
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-07-21 08:49:13 -0500

    Solve FizzBuzz in Python.

---
+++ python/FizzBuzz.py
@@ -0,0 +1,19 @@
+# FizzBuzz.py
+
+def main():
+    n = int(input("Please enter a positive integer (n > 0): "))
+    runFizzBuzz(n)
+
+def runFizzBuzz(n):
+    for i in range(1, n+1):
+        message = ""
+        if i % 3 == 0:
+            message += "Fizz"
+        if i % 5 == 0:
+            message += "Buzz"
+        if not message:
+            message = str(i)
+        print(message)
+
+if __name__ == "__main__":
+    main()
