a71f79a191c5734959ddcf7f8a833aa9dcb6ca7e
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-08-30 19:43:58 -0500

    For FizzBuzz in JavaScript, use prompt to get user input.

--- javascript/FizzBuzz.js
+++ javascript/FizzBuzz.js
@@ -1,5 +1,13 @@
+const prompt = require('prompt-sync')();
+
 function main () {
-    console.log("Bon weekend!");
+    console.log("It's time for FizzBuzz!");
+    const n = parseInt(prompt("Please enter a positive integer (n > 0): "));
+    runFizzBuzz(n);
+}
+
+function runFizzBuzz(n) {
+    console.log(`n = ${n}`);
 }
 
 if (require.main == module) {
