b8efc35b65d68883a25cf6ef1a5891f4e84abde6
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-08-31 09:00:26 -0500

    Solve FizzBuzz in JavaScript with a different method.

--- javascript/FizzBuzz.js
+++ javascript/FizzBuzz.js
@@ -3,10 +3,27 @@ const prompt = require('prompt-sync')();
 function main () {
     console.log("It's time for FizzBuzz!");
     const n = parseInt(prompt("Please enter a positive integer (n > 0): "));
-    runFizzBuzz(n);
+    runFizzBuzzV1(n);
 }
 
-function runFizzBuzz(n) {
+function runFizzBuzzV1(n) {
+    console.log(`n = ${n}`);
+    for (let i = 1; i <= n; i++) {
+        let message = "";
+        if (i % 3 == 0) {
+            message += "Fizz";
+        }
+        if (i % 5 == 0) {
+            message += "Buzz";
+        }
+        if (!message) {
+            message = i.toString();
+        }
+        console.log(message);
+    }
+}
+
+function runFizzBuzzV2(n) {
     console.log(`n = ${n}`);
     for (let i = 1; i <= n; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
