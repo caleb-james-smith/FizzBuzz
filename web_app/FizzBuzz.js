8414978f67d9c88584b3589ea9955c89f63ee69e
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-06-12 12:34:38 -0500

    Create simple web app (HTML/CSS/JS) for FizzBuzz with user input.

---
+++ web_app/FizzBuzz.js
@@ -0,0 +1,37 @@
+const numberInput = document.getElementById("number-input");
+const runButton = document.getElementById("run-button");
+const output = document.getElementById("output");
+
+function updateOutput() {
+    // Remove all children (nodes) of output element.
+    output.replaceChildren();
+
+    const number = numberInput.value;
+    if (number > 0) {
+       runFizzBuzz(number);
+    }
+}
+
+function runFizzBuzz(number) {
+    for (let i = 1; i <= number; i++) {
+        let message = "";
+        if (i % 3 === 0) {
+            message += "Fizz";
+        }
+        if (i % 5 === 0) {
+            message += "Buzz";
+        }
+        if (!message) {
+            message += i;
+        }
+        addParagraphToOutput(message);
+    }
+}
+
+function addParagraphToOutput(message) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = message;
+    output.appendChild(paragraph);
+}
+
+runButton.addEventListener("click", updateOutput);
